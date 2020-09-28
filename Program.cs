e83bc32ccf7e8d96a32fc8e023e83a2819410b87
tracht <marija.trachtenberg@gmail.com>
2020-09-28 10:59:15 +0100

    Reorganises methods inside Program.css

--- Program.cs
+++ Program.cs
@@ -4,10 +4,30 @@ namespace myNetApp
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void WorkingWithIntegers()
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("The current time is " + DateTime.Now); 
+            int a = 18; 
+            int b = 6;
+
+            // addition
+            int c = a - b;
+            Console.WriteLine(c);
+
+            // subtraction
+            c = a * b; 
+            Console.WriteLine(c);
+
+            // division 
+            c = a / b; 
+            Console.WriteLine(c);
+        }
+
+         static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            // Console.WriteLine("The current time is " + DateTime.Now); 
+            WorkingWithIntegers();
         }
     }
 }
