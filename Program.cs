128cc9be93699a5ebea939fe7e493113cc812f5b
tracht <marija.trachtenberg@gmail.com>
2020-09-28 11:01:13 +0100

    Adds OrderPrecedence method to Program.css

--- Program.cs
+++ Program.cs
@@ -23,11 +23,34 @@ namespace myNetApp
             Console.WriteLine(c);
         }
 
+        static void OrderPrecedence()
+        {
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
+
+            d = (a + b) * c;
+            Console.WriteLine(d);
+
+            d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            Console.WriteLine(d);
+
+            int e = 7;
+            int f = 4;
+            int g = 3;
+            int h = (e  + f) / g;
+            Console.WriteLine(h);
+        }
+
+
          static void Main(string[] args)
         {
             // Console.WriteLine("Hello World!");
             // Console.WriteLine("The current time is " + DateTime.Now); 
             WorkingWithIntegers();
+            OrderPrecedence();
         }
     }
 }
